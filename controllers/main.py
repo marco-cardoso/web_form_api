d7da70fe51e1efdcd725733b5e615cace09e26f0
Marco Cardoso <marcom.c@outlook.com>
2020-11-25 21:25:07 +0000

    Update main controller

--- controllers/main.py
+++ controllers/main.py
@@ -3,3 +3,12 @@ from flask import Blueprint
 
 app = Blueprint('main', __name__)
 
+
+@app.route("/health", methods=['GET'])
+def health():
+    if request.method == "GET":
+        return jsonify(
+            {
+                "message": "ok"
+            }
+        ), 200
