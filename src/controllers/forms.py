eab4414fc5a7a592a8eeb22390cdab441b6e616c
Marco Cardoso <marcom.c@outlook.com>
2020-11-26 02:32:39 +0000

    Form controller

--- src/controllers/forms.py
+++ src/controllers/forms.py
@@ -1,4 +1,31 @@
-from flask import request, jsonify
 from flask import Blueprint
+from flask import request, jsonify
+
+from src.database import forms, client
 
 app = Blueprint('forms', __name__)
+
+db = client.Database().db
+
+
+@app.route("/data", methods=["POST"])
+def insert_form():
+    if request.method == "POST":
+        form = request.get_json()
+        result = forms.insert(db, form)
+        return jsonify(
+            {
+                "id": str(result.inserted_id)
+            }
+        ), 201
+
+
+@app.route("/data/<form_id>", methods=["GET"])
+def get_form(form_id):
+    if request.method == "GET":
+        form = forms.find_by_id(db, form_id)
+        return jsonify(
+            {
+                "form": form
+            }
+        ), 200
